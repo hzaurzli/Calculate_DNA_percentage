f4f1ca14a6eebccb4c9a41417e6c43f2e6a1c0f0
Small runze <yun_act@163.com>
2022-07-04 17:46:45 +0800

    Update run_pbs.py

--- run_pbs.py
+++ run_pbs.py
@@ -16,7 +16,7 @@ for i in samples:
         line = line + "/data/home/huanfan/miniconda3/bin/python3 "
         line = line + "/data/home/huanfan/pycharm/venv/Percentage_DNA.py -wkdir ~/runze/test/ "
         line = line + "-ri ~/runze/ref/index/chloroplast -fq1 ~/runze/mix_cleandata/" + i + "_R1.fq.gz "
-        line = line + "-fq2 ~/runze/mix_cleandata/" + i + "_R2.fq.gz -k 8"
+        line = line + "-fq2 ~/runze/mix_cleandata/" + i + "_R2.fq.gz -k 9"
         w.write(line)
 
 
@@ -40,6 +40,6 @@ for i in samples:
         line = line + "/data/home/huanfan/pycharm/venv/Percentage_DNA.py -wkdir ~/runze/test_single/ "
         line = line + "-rg ~/runze/ref/" + i + '.fasta '
         line = line + "-ri "  + i + " -fq1 ~/runze/cleandata/" + i + "_R1.fq.gz "
-        line = line + "-fq2 ~/runze/cleandata/" + i + "_R2.fq.gz -k 8"
+        line = line + "-fq2 ~/runze/cleandata/" + i + "_R2.fq.gz -k 9"
         w.write(line)
 
